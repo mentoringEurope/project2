985efb90ab4e87237f198aaecfe300006c08fa17
tahirata83 <tahiratayev@ichc.ro>
2020-05-12 06:29:47 +0300

    first

---
+++ src/minNumber.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class minNumber {
+
+      /*
+        Create int 2D array
+
+        Which is {{1,2,3}, {2,3,1} , {5,5,5} , {2,1,3}}
+
+        print the min number from the 2d array
+     */
+
+
+     /*
+
+         {{1,2,3}, {2,3,1} , {5,5,5} , {2,1,3}}
+
+         int 2D arrayini  olustur
+
+        2D arrayinden min number print et
+
+     */
+
+
+
+
+    public static void main(String[] args) {
+
+
+}
+
+}
---
+++ src/reverseArr.java
@@ -0,0 +1,48 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class reverseArr {
+
+      /*
+        Given String array
+
+        reverse the String array
+
+        print the reversed array
+
+        NOTE: While printing the array dont use for loop
+        Use Arrays.toString(your_array_name);
+
+
+     */
+
+
+    /*
+
+          Verilen String array
+
+         String arrayi ters çevir
+
+         ters arrayi yazdır
+
+       NOT: arrayi print ederken for loop kullanmayın
+       Arrays.toString(your_array_name);  <--- kullanın
+
+     */
+
+
+    public static void main(String[] args) {
+
+        Scanner scan = new Scanner(System.in);
+
+        String myStr = scan.nextLine();
+
+        String[] useThisArray = myStr.split(" ");
+
+     //        code start here
+
+     //        Koda burdan başlayın
+
+    }
+
+}
